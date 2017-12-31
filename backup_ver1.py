ef5f3d61691002d0bcce552d36f8eefb9dc24e37
houbin-zh <houbin_zh@163.com>
2017-12-31 12:21:06 +0800

    version 1

--- backup_ver1.py
+++ backup_ver1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #-*-coding:utf-8-*-
 
 import os
@@ -27,35 +26,3 @@ if os.system(zip_command) == 0:
 else:
 	print('backup failed')
 	
-=======
-#-*-coding:utf-8-*-
-
-import os
-import time
-
-#需备份的目录
-source = ["E:\\work\\python\\backup_demo\\testFile\\testTxt.txt"]
-
-#主备份目录
-target_dir = 'E:\\work\\python\\backup_demo\\backup'
-
-#os.sep,路径分隔符，通用
-#time.strftime()，获取当前时间，并格式化
-target = target_dir + os.sep + \
-		time.strftime('%Y%m%d%H%M%S')+ '.zip'
-
-#备份目录不存在时，创建目录
-if not os.path.exists(target_dir):
-	os.mkdir(target_dir)
-
-zip_command = 'zip -r {0} {1}'.format(target,''.join(source))
-
-print('zip command is:')
-print(zip_command)
-print('running:')
-if os.system(zip_command) == 0:
-	print('successful backup to',target)
-else:
-	print('backup failed')
-	
->>>>>>> 备份程序
