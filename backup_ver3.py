106a28ef23fb9b721e9983a63e965a4daef1e3a8
houbin-zh <houbin_zh@163.com>
2018-01-01 14:06:34 +0800

    version 3

--- backup_ver3.py
+++ backup_ver3.py
@@ -9,7 +9,6 @@ source = ["E:\\work\\python\\backup_demo\\testFile\\testTxt.txt"]
 #主备份目录
 target_dir = 'E:\\work\\python\\backup_demo\\backup'
 
-#备份目录不存在时，创建目录
 if not os.path.exists(target_dir):
 	os.mkdir(target_dir)
 
